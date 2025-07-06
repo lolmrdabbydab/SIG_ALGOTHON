a1dad45d8b53e96f5b91e3db9fd349be77745002
Aadi566 <aaditya.singh5266@gmail.com>
2025-07-06 23:18:10 +0930

    fixed vol

--- main.py
+++ main.py
@@ -113,17 +113,17 @@ def calculatePerChange(new_val, old_val):
 def getVolatility(prices):
 
     price_history = prices[:, -(VOLATILITY_PERIOD + 1):]
+   
     volatility = np.zeros(nInst)
 
     for n, stock in enumerate(price_history):
         per_change_arr = np.zeros(VOLATILITY_PERIOD)
         for i in range(1, VOLATILITY_PERIOD + 1):
-            per_change_arr = calculatePerChange(stock[i], stock[i - 1])
+            per_change_arr[i - 1] = calculatePerChange(stock[i], stock[i - 1])
         
         volatility[n] = np.std(per_change_arr)
 
     return volatility
-
 
 def getMyPosition(prcSoFar):
     global currentPos
